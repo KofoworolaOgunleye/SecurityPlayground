252701b3dc255b9447b4b432be78347117a67eb2
Sam Price <samprice@Sams-MacBook-Pro.local>
2024-11-29 15:47:55 +0000

    feat(in progress): update the protected greeting endpoint to return a string of the OAuth2User attributes (not working)

--- src/main/java/com/example/SecurityPlayground/controller/SecurityPlaygroundController.java
+++ src/main/java/com/example/SecurityPlayground/controller/SecurityPlaygroundController.java
@@ -1,6 +1,9 @@
 package com.example.SecurityPlayground.controller;
 
+import com.example.SecurityPlayground.service.SecurityPlaygroundService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,16 +13,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1")
 public class SecurityPlaygroundController {
 
+    @Autowired
+    SecurityPlaygroundService securityPlaygroundService;
+
     @GetMapping("/open/greeting")
     public String openGreeting(){
         return "Dear user, welcome!";
     }
 
-    @GetMapping("/protected/greeting")
-    public String protectedGreeting(@AuthenticationPrincipal OAuth2User principal){
-        String name= principal.getAttributes().get("id").toString();
-        String username = principal.getAttributes().get("login").toString();
+//    @GetMapping("/protected/greeting")
+//    public String protectedGreeting(@AuthenticationPrincipal OAuth2User principal){
+//
+//        return securityPlaygroundService.protectedGreeting(principal);
+//    }
 
-        return "Hiiiii! Welcome "+ username + " " +"(" +name + ")"+"!";
+    @GetMapping("/protected/greeting")
+    public String protectedGreeting(OAuth2UserRequest request){
+
+        return securityPlaygroundService.loadUser(request).getAttributes().toString();
     }
 }
